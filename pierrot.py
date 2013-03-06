2c00b6422a02a44e4b21183b012bf9b33484fbd8
jordanrossdunn <ortalismotmot@gmail.com>
2013-03-05 23:00:36 -0800

    improves base value entry and subsequent action selection in primary rhythms input

--- pierrot.py
+++ pierrot.py
@@ -10,6 +10,7 @@ import mingus.core.value as value
 from rhythms import *
 
 import string
+from fractions import Fraction
 
 #def main():
 """documentatioin/summary"""
@@ -40,7 +41,7 @@ print "\n" + string.center(" Primary Pitch-Class Sets ", width, "~")
 
 while True:
 
-	raw = raw_input("\nEnter pitch-class set " + str(len(primary_pc_sets)) + ": ")
+	raw  = raw_input("\nEnter pitch-class set " + str(len(primary_pc_sets)) + ": ")
 	temp = str(PcSet(raw))
 
 	#check for valid pitch-class set entry
@@ -54,7 +55,7 @@ while True:
 	for i in range(len(temp)):
 		member.append(temp[i])
 	for index, item in enumerate(member):
-		if item == 'A':
+		if   item == 'A':
 			member[index] = 10
 		elif item == 'B':
 			member[index] = 11
@@ -65,9 +66,9 @@ while True:
 	temp = PcSet(raw).normal()
 
 	#compensate for bugs in normal()
-	if len(str(temp)) == 12:
+	if   len(str(temp)) == 12:
 		temp = (temp.zero()).transpose(member[0])
-	elif (len(str(temp)) == 2) and ((member[1]-member[0])%12 == 6):
+	elif len(str(temp)) == 2 and (member[1]-member[0])%12 == 6:
 		temp = temp.transpose(6)
 
 	#check against previous input for equivalent normal and/or prime forms
@@ -80,20 +81,20 @@ while True:
 		if str(temp.prime()) == str(item.prime()):
 			equivalent_prime.append(index)
 
-	resp = "y"
+	resp = 'y'
 	if int(len(equivalent_normal)) > 0:
 
 		if int(len(equivalent_normal)) == 1:
-			print "\nEquivalent normal and prime forms with entry " + str(equivalent_normal)
+			print "\nEquivalent normal and prime forms with entry "   + str(equivalent_normal)
 		else:
-			print "\nEquivalent normal form prime forms with entries " + str(equivalent_normal)
+			print "\nEquivalent normal and prime forms with entries " + str(equivalent_normal)
 
 		resp = raw_input("\nAllow entry? (y/n): ")
 
 	elif int(len(equivalent_prime)) > 0:
 
 		if int(len(equivalent_prime)) == 1:
-			print "\nEquivalent prime form with entry " + str(equivalent_prime)
+			print "\nEquivalent prime form with entry "   + str(equivalent_prime)
 		else:
 			print "\nEquivalent prime form with entries " + str(equivalent_prime)
 		
@@ -104,16 +105,16 @@ while True:
 		primary_pc_sets.append(temp)
 
 	#display current primary set collection data
-	print "\n" + string.ljust("#", 15) + string.ljust("Normal Form", 25),
-	print string.ljust("Prime Form", 25) + string.ljust("Interval Vector", 20)
-	print "-" * width
+	print "\n" + string.ljust("#", 5) + string.ljust("Normal Form", 25),
+	print string.ljust("Prime Form", 25) + string.ljust("Interval Vector", 25)
+	print "-" * int(width)
 	for index, item in enumerate(primary_pc_sets):
-		print string.ljust(str(index), 15) + string.ljust(str(item), 25),
-		print string.ljust(str(item.prime()), 25) + string.ljust(str(item.ivec()), 20)
+		print string.ljust(str(index), 5) + string.ljust(str(item), 25),
+		print string.ljust(str(item.prime()), 25) + string.ljust(str(item.ivec()), 25)
 	
 	#continue or break?
 	again = raw_input("\nEnter another set? (y/n): ")
-	while again != "y" and again != "n":
+	while again != 'y' and again != 'n':
 		again = raw_input("\nEnter another set? (y/n): ")
 	if again == 'n':
 		break
@@ -124,16 +125,17 @@ while True:
 
 #get primary rhythms input
 
-relative_durations = get_relative_durations()
-
-options = []
-for key, item in relative_durations.iteritems():
-	print key
-	options.append(key)
-options.sort()
-
-
 print "\n" + string.center(" Primary Rhythms ", width, "~")
+
+actions = 	[
+				'Use base value',
+				'Make single dotted',
+				'Make double dotted',
+				'Make triple dotted',
+				'Make triplet',
+				'Make quintuplet',
+				'Make septuplet'
+			]
 
 while True:
 
@@ -143,25 +145,55 @@ while True:
 
 	while True:
 
-		print "\nBase values:" + str(relative_durations['base'])
+		#select a base value
+		print "\nBase note values: "
+		print "\n" + string.ljust("#", 5) + string.ljust("Value", 15)
+		print "-" * int(width)
+		for index, item in enumerate(value.base_values):
+			value_as_ratio = str(Fraction(1.0/item))
+			print string.ljust(str(index), 5) + string.ljust(value_as_ratio, 15)
 
-		raw = raw_input("\nEnter base value " + str(len(rhythm)) + ": ")
-		temp = float(raw)
-
-		
-		if(temp in value.base_values):
-			print "\nOptions:"
-			"""
-			for key, item in relative_durations.iteritems():
-				print string.ljust(key, 15)
-			"""
-			for index, item in enumerate(options):
-				print string.ljust(str(index), 5) + string.ljust(item, 15)
-			raw = raw_input("\nSelect an option: ")
-
+		index = raw_input("\nSelect value " + str(len(rhythm)) + ": ")
+		if(int(index) in range(len(value.base_values))):
+			val = value.base_values[int(index)]
 		else:
-			print "\nInvalid duration"
+			print "\nInvalid selection"
 			continue
+		
+		#select an action
+		base_index = value.base_values.index(val)
+		print "\nActions:"
+		print "\n" + string.ljust("#", 5) + string.ljust("Action", 15)
+		print "-" * int(width)
+		for index, item in enumerate(actions):
+			print string.ljust(str(index), 5) + string.ljust(item, 15) 
+		
+		action = raw_input("\nSelect an action: ")
+		while int(action) not in range(len(actions)):
+			action = raw_input("\nSelect an action: ")
+
+		#option actions
+		if   action == 0:
+			#use base value
+			print "Action " + str(action)
+		elif action == 1:
+			#make single dotted
+			print "Action " + str(action)
+		elif action == 2:
+			#make double dotted
+			print "Action " + str(action)
+		elif action == 3:
+			#make triple dotted
+			print "Action " + str(action)
+		elif action == 4:
+			#make triplet
+			print "Action " + str(action)
+		elif action == 5:
+			#make quintuplet
+			print "Action " + str(action)
+		elif action == 6:
+			#make septuplet
+			print "Action " + str(action)
 
 		#continue or break?
 		again = raw_input("\nEnter another value? (y/n): ")
